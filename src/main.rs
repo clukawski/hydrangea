f33418dacad89f9b1376479e43beeb72da32aabf
Conrad Lukawski <conrad@backlight.ca>
2020-12-03 01:20:53 -0500

    theo and work on link()

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 extern crate failure;
 extern crate futures;
 extern crate irc;
+extern crate linkify;
 extern crate pickledb;
 extern crate radix64;
+extern crate rand;
 extern crate serde;
 
 use futures::prelude::*;
 use irc::client::prelude::*;
+use linkify::LinkFinder;
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
 use radix64::STD;
+use rand::seq::IteratorRandom; // 0.7.3
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 #[derive(Serialize, Deserialize)]
 struct Smoker {
@@ -22,6 +31,7 @@ const USERNAME: &'static str = "pybot-rs";
 const CHANNELS: &'static [&'static str] = &["#darwin", "#rosegold"];
 const NETWORK: &'static str = "irc.darwin.network";
 const PASSWORD: &'static str = "";
+const FILENAME: &str = "/home/conrad/theo";
 
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
@@ -67,6 +77,8 @@ async fn main() -> Result<(), failure::Error> {
             authenticate(&client, &message, &mut authenticated)?;
             abuse(&client, &message)?;
             smoke(&client, &message, &mut db)?;
+            // link(&message)?;
+            theo(&client, &message)?;
         }
         retry_count = retry_count - 1;
         if retry_count == 0 {
@@ -113,8 +125,16 @@ fn abuse(
         message.to_string().contains(":abuse daddy") || message.to_string().contains(":abuse evan");
     let shivaram = message.to_string().contains(":abuse shivaram");
     let vivi = message.to_string().contains(":abuse vivi");
+    let comradeblue = message.to_string().contains(":abuse comradeblue");
+    let wrmsr = message.to_string().contains(":abuse wrmsr");
+    let ed = message.to_string().contains(":abuse ed");
+    let carmen = message.to_string().contains(":abuse carmen");
     let pybot = message.to_string().contains(&pybotstring);
     let msgstr = message.to_string();
+
+    let mut abuse_presets = HashMap::new();
+    abuse_presets.insert(String::from("evan"), String::from("evan"));
+
     if evan {
         let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
         let username = splitmsg[1];
@@ -133,14 +153,71 @@ fn abuse(
         let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
         let username = splitmsg[1];
         let trimmed = username.trim();
+
+        if rand::random() {
+            client
+                .send_privmsg(channel, format!("{} loves plan 9 C", trimmed))
+                .unwrap();
+        } else {
+            client
+                .send_privmsg(
+                    channel,
+                    format!(
+                        "{} doesn't believe in the importance of american military hegemony",
+                        trimmed
+                    ),
+                )
+                .unwrap();
+        }
+    }
+    if comradeblue {
+        let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
+        let username = splitmsg[1];
+        let trimmed = username.trim();
         client
-            .send_privmsg(channel, format!("{} loves plan 9 C", trimmed))
+            .send_privmsg(channel, format!("{} isn't a real programmer", trimmed))
+            .unwrap();
+    }
+    if wrmsr {
+        let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
+        let username = splitmsg[1];
+        let trimmed = username.trim();
+        client
+            .send_privmsg(channel, format!("{} #1 nancy pelosi fan", trimmed))
+            .unwrap();
+    }
+    if ed {
+        let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
+        let username = splitmsg[1];
+        let trimmed = username.trim();
+        client
+            .send_privmsg(
+                channel,
+                format!("\x0352{}: ARE THOSE FEATURES DONE YET??? HOW ARE YOUR OKRs LOOKING? Look, we're going to need you to stack rank your team mmmmmmkayyy?\x03", trimmed),
+            )
             .unwrap();
     }
     if pybot {
         client.send_privmsg(channel, "sux to suck, luser").unwrap();
     }
-    if message.to_string().contains(":abuse") && !evan && !vivi && !pybot && !shivaram {
+    if carmen {
+        let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
+        let username = splitmsg[1];
+        let trimmed = username.trim();
+        client
+            .send_privmsg(channel, format!("\x0375{} loves android\x03", trimmed))
+            .unwrap();
+    }
+    if message.to_string().contains(":abuse")
+        && !evan
+        && !vivi
+        && !pybot
+        && !shivaram
+        && !comradeblue
+        && !wrmsr
+        && !ed
+        && !carmen
+    {
         let splitmsg: Vec<&str> = msgstr.split(&splitstring).collect();
         let username = splitmsg[1];
         let trimmed = username.trim();
@@ -188,6 +265,32 @@ fn smoke(
     Ok(())
 }
 
+fn theo(
+    client: &irc::client::Client,
+    message: &irc::proto::Message,
+) -> std::result::Result<(), failure::Error> {
+    let channel = get_channel(message);
+    let theo_pattern = format!("PRIVMSG {} theo", channel);
+    println!("{}", theo_pattern);
+    let theo = message.to_string().contains(&theo_pattern.to_string());
+
+    if theo {
+        client.send_privmsg(channel, format!("theo: {}", find_theo().to_string()))?;
+    }
+
+    Ok(())
+}
+
+// TODO
+fn link(message: &irc::proto::Message) -> std::result::Result<(), failure::Error> {
+    let finder = LinkFinder::new();
+    let msg = &message.to_string();
+    let links: Vec<_> = finder.links(msg).collect();
+    println!("{:?}", links);
+
+    Ok(())
+}
+
 fn get_channel(message: &irc::proto::Message) -> &str {
     for channel in CHANNELS.iter() {
         if message.to_string().contains(channel) {
@@ -195,4 +298,16 @@ fn get_channel(message: &irc::proto::Message) -> &str {
         }
     }
     ""
+}
+
+fn find_theo() -> String {
+    let f = File::open(FILENAME)
+        .unwrap_or_else(|e| panic!("(;_;) file not found: {}: {}", FILENAME, e));
+    let f = BufReader::new(f);
+
+    let lines = f.lines().map(|l| l.expect("Couldn't read line"));
+
+    lines
+        .choose(&mut rand::thread_rng())
+        .expect("File had no lines")
 }
